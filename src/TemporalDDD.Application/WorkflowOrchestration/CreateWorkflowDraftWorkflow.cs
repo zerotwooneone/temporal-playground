12ad371323dcd6dcb558d50b6836865eb5078ef2
zero <squiredd@gmail.com>
2026-08-10 10:21:10 -0700

    fix workflow definition

--- src/TemporalDDD.Application/WorkflowOrchestration/CreateWorkflowDraftWorkflow.cs
+++ src/TemporalDDD.Application/WorkflowOrchestration/CreateWorkflowDraftWorkflow.cs
@@ -5,13 +5,6 @@ namespace TemporalDDD.Application.WorkflowOrchestration;
 [Workflow]
 public class CreateWorkflowDraftWorkflow
 {
-    private readonly IWorkflowOrchestrationActivities _activities;
-
-    public CreateWorkflowDraftWorkflow(IWorkflowOrchestrationActivities activities)
-    {
-        _activities = activities;
-    }
-
     [WorkflowRun]
     public async Task RunAsync(CreateWorkflowDraftInput input)
     {
@@ -22,13 +15,13 @@ public class CreateWorkflowDraftWorkflow
 
         // Execute CreateDraftAndSaveAsync activity
         var saveResult = await Workflow.ExecuteActivityAsync(
-            () => _activities.CreateDraftAndSaveAsync(input),
+            (IWorkflowOrchestrationActivities activities) => activities.CreateDraftAndSaveAsync(input),
             activityOptions);
 
         // Execute PublishApplicationEventsAsync activity with the events from the first activity
         var publishInput = new PublishEventsInput(saveResult.Events);
         await Workflow.ExecuteActivityAsync(
-            () => _activities.PublishApplicationEventsAsync(publishInput),
+            (IWorkflowOrchestrationActivities activities) => activities.PublishApplicationEventsAsync(publishInput),
             activityOptions);
     }
 }
--- src/TemporalDDD.Application/WorkflowOrchestration/UpdateWorkflowNodesWorkflow.cs
+++ src/TemporalDDD.Application/WorkflowOrchestration/UpdateWorkflowNodesWorkflow.cs
@@ -5,13 +5,6 @@ namespace TemporalDDD.Application.WorkflowOrchestration;
 [Workflow]
 public class UpdateWorkflowNodesWorkflow
 {
-    private readonly IWorkflowOrchestrationActivities _activities;
-
-    public UpdateWorkflowNodesWorkflow(IWorkflowOrchestrationActivities activities)
-    {
-        _activities = activities;
-    }
-
     [WorkflowRun]
     public async Task RunAsync(UpdateWorkflowNodesInput input)
     {
@@ -22,13 +15,13 @@ public class UpdateWorkflowNodesWorkflow
 
         // Execute UpdateNodesAndSaveAsync activity
         var saveResult = await Workflow.ExecuteActivityAsync(
-            () => _activities.UpdateNodesAndSaveAsync(input),
+            (IWorkflowOrchestrationActivities activities) => activities.UpdateNodesAndSaveAsync(input),
             activityOptions);
 
         // Execute PublishApplicationEventsAsync activity with the events from the first activity
         var publishInput = new PublishEventsInput(saveResult.Events);
         await Workflow.ExecuteActivityAsync(
-            () => _activities.PublishApplicationEventsAsync(publishInput),
+            (IWorkflowOrchestrationActivities activities) => activities.PublishApplicationEventsAsync(publishInput),
             activityOptions);
     }
 }
