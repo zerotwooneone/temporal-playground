32e54d0dd1e92a229cbc1e19a36bef50ceb10185
zero <squiredd@gmail.com>
2026-08-10 09:54:13 -0700

    config vite to use api port

--- src/TemporalDDD.React.UI/TemporalDDD.React.UI/vite.config.ts
+++ src/TemporalDDD.React.UI/TemporalDDD.React.UI/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:7049', // Or http://localhost:5213
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
